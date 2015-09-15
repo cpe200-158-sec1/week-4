2d6dcc1bdbd68425fd9c319dce55314c8c50b197
Watchanan Chanthaphakun <tmwatchanan@gmail.com>
2015-09-15 17:52:28 +0700

    Lab401 v.1
    !! Point.cs
    + Added _x and _y private attributes.
    + Created get-set properties for _x and _y.
    + Made Point constructor with 3 overloading.
    + Overrided ToString method to display Point object.
    
    !! Circle.cs
    + Inherited Point class.
    + Added _center and _radius private attributes.
    + Created get-set properties.
    + Implemented 4 constructors with overloading.
    + Overried ToString method.
    + Implemented setCircle and getArea methods.
    
    !! Cylinder.cs
    + Inherited Circle class.
    + Added _surface private (Circle object) attibute.
    + Added _height private double attribute.
    + Implemented 5 constructors with overloading.
    ! Override ToString method.
    ! Override getArea method from virtual getArea method in Circle class.
    + Implemented getVolume method.

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,73 @@
 
 namespace Lab4
 {
-	public class Cylinder
-	{
-		public Cylinder ()
-		{
-		}
-	}
+    public class Cylinder : Circle
+    {
+        private Circle _surface;
+        private double _height;
+        public Circle Surface
+        {
+            get;
+            set;
+        }
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _height = value;
+                }
+                else
+                {
+                    _height = 1;
+                }
+            }
+        }
+
+        public Cylinder()
+        {
+            Surface = new Circle();
+            Height = 1.0;
+        }
+        public Cylinder(double pRadius, double pHeight)
+        {
+            Surface = new Circle(pRadius);
+            Height = pHeight;
+        }
+        public Cylinder(double pX, double pY, double pRadius, double pHeight)
+        {
+            Surface = new Circle(pX, pY, pRadius);
+            Height = pHeight;
+        }
+        public Cylinder(Circle pCircle)
+        {
+            Surface = new Circle(pCircle.Center.X, pCircle.Center.Y, pCircle.Radius);
+            Height = 1.0;
+        }
+        public Cylinder(Cylinder pCylinder)
+        {
+            Surface = new Circle(pCylinder.Surface.Center.X, pCylinder.Surface.Center.Y, pCylinder.Surface.Radius);
+            Height = pCylinder.Height;
+        }
+
+        public override double getArea()
+        {
+            double pi = Math.PI;
+            return ((2.0 * Surface.getArea()) + (2 * (pi) * Surface.Radius * Height));
+        }
+        public double getVolume()
+        {
+            return (Surface.getArea() * Height);
+        }
+        public override string ToString()
+        {
+            return string.Format("[Cylinder: center{0}, radius={1}, height={2}, surface={3}, volume={4}]", Surface.Center, Surface.Radius, Height, getArea(), getVolume());
+        }
+    }
 }
 
--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -4,9 +4,39 @@ namespace Lab4
 {
 	public class Point
 	{
-		public Point ()
+        private double _x;
+        private double _y;
+        public double X
+        {
+            get;
+            set;
+        }
+        public double Y
+        {
+            get;
+            set;
+        }
+
+        public Point ()
 		{
+            X = 0;
+            Y = 0;
 		}
-	}
+        public Point (Point pPoint)
+        {
+            X = pPoint.X;
+            Y = pPoint.Y;
+        }
+        public Point (double pX, double pY)
+        {
+            X = pX;
+            Y = pY;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", X, Y);
+        }
+    }
 }
 
