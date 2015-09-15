2762e5ac5ec770a893862580fa9fae133e055709
thawanrat2609 <only_amp_me@hotmail.com>
2015-09-15 18:50:29 +0700

    570610576 lab4-1

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,78 @@
 
 namespace Lab4
 {
-	public class Cylinder
+	public class Cylinder : Circle
 	{
-		public Cylinder ()
+        protected double _height;
+
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (_height >= 0)
+                {
+                    _height = value;
+                }
+                else
+                {
+                    _height = 1.0;
+                }
+            }
+        }
+
+        public Cylinder () :base()
 		{
+            _height = 1.0;
 		}
-	}
+
+        public Cylinder(double r4, double h1) 
+        {
+            _radius = r4;
+            _height = h1;
+        }
+
+        public Cylinder(double x, double y, double r, double h)
+        {
+            _height = h;
+            _radius = r;
+            _x = x;
+            _y = y;
+        }
+
+        public Cylinder(Circle cpy)
+        {
+            _x = cpy.X;
+            _y = cpy.Y;
+            _height = 1.0;
+            _radius = cpy.Radius;
+        }
+
+        public Cylinder(Cylinder cc)
+        {
+            _x = cc.X;
+            _y = cc.Y;
+            _radius = cc.Radius;
+            _height = cc.Height;
+        }
+
+        public double getSurface()
+        {
+            return (2 * (Math.PI) * _radius * _height + 2 * (22/7) * _radius * _radius);
+        }
+
+        public double getVolume()
+        {
+            return ((Math.PI) * _radius * _radius * _height);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Cylinder: center=({0},{1}), radius={2}, height={3}, surface={4}, volume={5}]" , _x, _y, _radius, _height, getSurface(), getVolume());
+        }
+    }
 }
 
--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -4,9 +4,59 @@ namespace Lab4
 {
 	public class Point
 	{
-		public Point ()
+        protected double _x;
+        protected double _y;
+
+        public double X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+
+        public Point ()
 		{
-		}
-	}
+            _x = 0.0;
+            _y = 0.0;
+        }
+
+        public Point(Point cpy)
+        {
+            _x = cpy._x;
+            _y = cpy._y;
+        }
+
+        public Point(double x1,double y1)
+        {
+            _x = x1;
+            _y = y1;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})" , X , Y );
+        }
+
+
+
+
+    }
 }
 
