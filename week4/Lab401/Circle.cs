e6440c9b7b0ba951af2250ef4e552d4ffa3c3c7f
trewzaki <tongtananut@gmail.com>
2015-09-15 17:29:44 +0700

    570610574 Lab401

--- week4/Lab401/Circle.cs
+++ week4/Lab401/Circle.cs
@@ -2,11 +2,56 @@
 
 namespace Lab4
 {
-	public class Circle
-	{
-		public Circle ()
-		{
-		}
-	}
+    public class Circle : Point
+    {
+        private Point _center;
+        private double _radius;
+
+        public override string ToString()
+        {
+            return string.Format("Circle: center({0},{1}), radius={2}, area={3},", _center.X, _center.Y, _radius, getArea());
+        }
+        public Circle()
+        {
+            _center = new Point();
+            _radius = 1;
+        }
+        public Circle(Circle obj)
+        {
+            _center = new Point(obj.Center); // not sure
+            _radius = obj.Radius;
+        }
+        public Circle(double x, double y, double radius)
+        {
+            _center = new Point(x, y);
+            _radius = radius;
+        }
+        public Circle(double radius)
+        {
+            _center = new Point();
+            _radius = radius;
+        }
+
+        public void setCircle(double x, double y, double radius)
+        {
+            this._center = new Point(x, y);
+            this._radius = radius;
+        }
+        public double getArea()
+        {
+            return 3.14159265358979 * _radius * _radius;
+        }
+        public double Radius
+        {
+            get { return _radius; }
+            set { _radius = value; } 
+        }
+        public Point Center
+        {
+            get { return _center; }
+            set { _center = value; }
+        }
+
+    }
 }
 
--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,58 @@
 
 namespace Lab4
 {
-	public class Cylinder
+	public class Cylinder : Circle
 	{
-		public Cylinder ()
+        private double _height;
+        private Circle _circle;
+
+        public override string ToString()
+        {
+            return string.Format("Cylinder: center({0},{1}), radius={2}, height={3}, surface={4}, volume={5}]", _circle.Center.X, _circle.Center.Y, _circle.Radius, _height, getArea(), getVolume());
+        }
+        public Cylinder ()
 		{
+            _circle = new Circle();
+            _height = 1;
 		}
-	}
+        public Cylinder(double radius, double height)
+        {
+            _circle = new Circle(radius);
+            _height = height;
+        }
+        public Cylinder(double x, double y, double radius, double height)
+        {
+            _circle = new Circle(x, y, radius);
+            _height = height;
+        }
+        public Cylinder (Circle obj)
+        {
+            _circle = new Circle(obj);
+            _height = 1;
+        }
+        public Cylinder (Cylinder obj)
+        {
+            _circle = new Circle(obj._circle.Center.X, obj._circle.Center.Y, obj._circle.Radius);
+            _height = obj._height;
+        }
+        public double getArea()
+        {
+            return ((2 * 3.14159265358979 * _circle.Radius * _height) + (2 * _circle.getArea()));
+        }
+        public double getVolume()
+        {
+            return _circle.getArea() * Height;
+        }
+        public double Height
+        {
+            get { return _height; }
+            set {
+                if (value == 0)
+                    _height = 1;
+                else
+                    _height = value;
+            }
+        }
+    }
 }
 
