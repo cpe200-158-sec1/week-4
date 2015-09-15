af95b4837d35da95d757e845f35d4ed295378fa4
kaiyast <0kaiyast0@gmail.com>
2015-09-15 16:26:38 +0700

    570610549 Lab401

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,76 @@
 
 namespace Lab4
 {
-	public class Cylinder
+	public class Cylinder : Circle
 	{
-		public Cylinder ()
+        private double _height;
+        Circle c = new Circle();
+        
+        public double height
+        {
+            get { return _height; }
+            set
+            {
+                if (value < 0)
+                {
+                    _height = 0;
+                }
+                else
+                {
+                    _height = value;
+                }
+            }
+        }
+
+
+        public Cylinder ()
 		{
+
+            c = new Circle();
+            _height = 1.0;
 		}
-	}
+
+        public Cylinder(double r, double h)
+        {
+            c.radius = r;
+            _height = h;
+        }
+
+        public Cylinder(double x, double y , double r, double h)
+        {
+            c = new Circle(x,y, r);
+            _height = h;
+        }
+
+        public Cylinder(Circle c)
+        {
+            this.c = c;
+            _height = 1.0;
+        }
+
+        public Cylinder(Cylinder inputcylinder)
+        {
+            c = new Circle(inputcylinder.c);
+            _height = inputcylinder.height;
+        }
+
+         new public  double getArea()
+        {
+            return c.getArea()*2 + (_height*c.radius*2*Math.PI);
+        }
+
+        public double getVolume()
+        {
+            return c.getArea() * _height;
+        }
+
+        public override string ToString()
+        {
+            //[Cylinder: center(1,1), radius=3, height=4, surface=131.946891450771, volume=113.097335529233]
+            string outputstring = "[Cylinder: center(";
+            outputstring += c.center.x + "," + c.center.y + "), radius=" + c.radius + ", height=" + _height + ", surface=" + getArea() + ", volume=" + getVolume()+"]";
+            return outputstring;
+        }
+    }
 }
 
--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -4,9 +4,56 @@ namespace Lab4
 {
 	public class Point
 	{
-		public Point ()
+        private double _x,_y;
+
+        public double x
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+
+        public double y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+
+
+        public Point ()
 		{
+            _x = 0;
+            _y = 0;
 		}
-	}
+
+        public Point(Point p )
+        {
+            _x = p.x;
+            _y = p.y;
+        }
+
+        public Point(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public override string ToString()
+        {
+            string outputstring = "";
+            return outputstring;
+        }
+    }
 }
 
