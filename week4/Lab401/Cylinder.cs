32a6a81389a834a43aaef70366e5219c503c3543
drivemontri <auntafung@hotmail.com>
2015-09-15 18:21:25 +0700

    570610694 Lab 4.1

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using Lab4;
 
-namespace Lab4
+namespace Lab4 
 {
-	public class Cylinder
+	public class Cylinder : Circle
 	{
-		public Cylinder ()
-		{
-		}
-	}
+        private double _height;
+        private Circle _surface;
+
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _height = value;
+                }
+                else _height = 1;
+            }
+        }
+
+        public Circle Surface
+        {
+            get
+            {
+                return _surface;
+            }
+            set
+            {
+                _surface = value;
+            }
+        }
+
+        public Cylinder()
+        {
+            Surface = new Circle();
+            Height = 1;        
+        }
+
+        public Cylinder(double a , double b)
+        {   
+            Surface = new Circle(a);
+            Height = b;
+        }
+
+        public Cylinder(double a, double b, double c, double d)
+        {
+            Surface = new Circle(a,b,c);
+            Height = d;
+        }
+
+        public Cylinder(Circle a)
+        {
+            this.Surface = a;
+            Height = 1;
+        }
+
+        public Cylinder(Cylinder a)
+        {
+            this.Surface = a.Surface;
+            this.Height = a.Height;
+        }
+
+        public double getArea()
+        {
+            return (2 * Surface.getArea()) + (Height * 2 * Math.PI * Surface.Radius);
+        }
+
+        public double getVolume()
+        {
+            return Surface.getArea() * Height;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Cylinder: {0}, radius={1}, height{2}, surface={3}, volume={4}", Surface.Center, Surface.Radius, Height, getArea(), getVolume());
+        }
+
+
+    }
 }
 
--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -4,9 +4,52 @@ namespace Lab4
 {
 	public class Point
 	{
-		public Point ()
-		{
-		}
+            private double _x, _y;
+
+            public double X
+            {
+                get
+                {
+                    return _x;
+                }
+                set
+                {
+                    _x = value;
+                }
+             }
+            
+            public double Y
+            {
+                get
+                {
+                    return _y;
+                }
+                set
+                {
+                    _y = value;
+                }
+            }
+            public Point()
+            {
+                 X = Y = 0;
+            }
+
+            public Point(double x = 0,double y = 0)
+            {
+                 X = x;
+                 Y = y;
+            }
+
+            public Point(Point a)
+            {
+                this.X = a.X;
+                this.Y = a.Y;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("center : ({0},{1})", X, Y);
+            }
+
 	}
 }
-
