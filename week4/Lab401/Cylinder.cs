a6e225b1e6ea697f7046e452b7165e996f1031bd
attaphon1996 <attaphon_wong@cmu.ac.th>
2015-09-15 18:20:23 +0700

    lab 401 570610571

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,78 @@
 
 namespace Lab4
 {
-	public class Cylinder
+	public class Cylinder :Circle
 	{
-		public Cylinder ()
-		{
-		}
-	}
-}
+		private double _height;
 
+		//Properties
+		public double height
+		{ 
+			get{ return _height;}
+			set{ _height = (value >= 0)? value : 1.00 ;}
+		} 
+
+
+		//Constructor
+		public Cylinder()
+		{
+			height = 1.00;
+			radius = 1.00;
+			x = 0.00;
+			y = 0.00;
+		}
+		public Cylinder(double r, double h)
+		{
+			height = h;
+			radius = r;
+			x = 0.00;
+			y = 0.00;
+		}
+		public Cylinder(double xx ,double yy ,double r ,double h)
+		{
+			height = h;
+			radius = r;
+			x = xx;
+			y = yy;
+		}
+		public Cylinder(Circle a)
+		{
+			height = 1.00;
+			radius = a.radius;
+			x = a.x;
+			y = a.y;
+		}
+		public Cylinder(Cylinder a)
+		{
+			height = a.height;
+			radius = a.radius;
+			x = a.x;
+			y = a.y;
+		}
+
+		//method
+
+		public override double getArea()
+		{
+			return (2.00 * Math.PI * radius * height) + (2.00 * Math.PI * radius * radius);
+		}
+		public double getVolume()
+		{
+			return Math.PI * radius * radius * height;
+		}
+		public override string ToString()
+		{
+			return String.Format ("[Cylinder: center({0},{1}), radius={2}, height={3}, surface={4}, volume={5}]"
+				, this.x, this.y, this.radius, this.height, this.getArea (), this.getVolume ());
+
+		
+		}
+
+			
+
+
+ 		
+		
+	}
+
+}
--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -4,9 +4,42 @@ namespace Lab4
 {
 	public class Point
 	{
-		public Point ()
-		{
+		private double _x , _y;
+
+		//propeties
+		public double x {
+			get { return _x; }
+			set { _x = value; }
 		}
+		public double y {
+			get { return _y; }
+			set{ _y = value; }
+		}
+
+
+		//constructor
+		public Point()
+		{
+			x = 0.0;
+			y = 0.0;
+		}
+		public Point(double xx , double yy)
+		{
+			x = xx;
+			y = yy;
+		}
+		public Point(Point a)
+		{
+			x = a.x;
+			y = a.y;
+		}
+
+		//Methods
+		public override string ToString()
+		{
+			return String.Format ("center({0},{1})", x, y);
+		}
+			
+
 	}
 }
-
