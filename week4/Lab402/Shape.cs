0c3bad158ab2cb2bafe8f8429bc87847b6b6dfb4
Jirayut Keawchuen <jirayut_deknoi@hotmail.co.th>
2015-09-15 17:27:36 +0700

    570610558 lab 402 (ยังไม่สร็จ)

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -4,9 +4,18 @@ namespace Lab4
 {
 	public abstract class Shape
 	{
-		public Shape ()
+        public string _Color="gold";
+
+
+        public string Color
+        {
+            get { return _Color; }
+        }
+		public Shape (string c)
 		{
+            _Color = c;
 		}
+
 	}
 }
 
