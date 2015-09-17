3b8ce884ff6b7817a511279d3d2ccafe74ef76e1
ai2mi <h2o_coke@hotmail.com>
2015-09-17 16:24:12 +0700

    0571 lab402

--- week4/Lab402/Rectangle.cs
+++ week4/Lab402/Rectangle.cs
@@ -2,11 +2,73 @@
 
 namespace Lab4
 {
-	public class Rectangle
+	public class Rectangle : Shape
 	{
+		private double _height, _width;
+
+
+		//propeties
+		public double width {
+			get{ return _width; }
+			set{ _width = (value >= 0) ? value : 1.00; }
+		}
+		public double height {
+			get{ return _height; }
+			set{ _height = (value >= 0) ? value : 1.00; }
+		}
+		public override string color
+		{
+			get{ return _color;}
+		}
+
+
+		// constructor
 		public Rectangle ()
 		{
+			width = 1.00;
+			height = 1.00;
+			_color = "gold";
 		}
+		public Rectangle(string c,double w ,double h)
+		{
+			width = w;
+			height = h;
+			_color = c;
+		}
+		
+		public Rectangle (Rectangle a)
+		{
+			width = a.width;
+			height = a.height;
+			_color = a.color;
+		}
+
+		//method
+		public override double getArea()
+		{
+			return this.width * this.height;
+		}
+		public override double getPerimeter()
+		{
+			return (this.width * 2) + (this.height * 2);
+		}
+
+
+
+		public override string ToString ()
+		{
+			return string.Format ("[Rectangle: width={0}, height={1}, area={2}, color={3}]",
+				this.width, this.height, this.getArea(), this.color);
+			
+		} 
+
+
+
+
+
+
+
+
 	}
 }
 
--- week4/Lab402/Triangle.cs
+++ week4/Lab402/Triangle.cs
@@ -2,11 +2,61 @@
 
 namespace Lab4
 {
-	public class Triangle
+	public class Triangle :Shape
 	{
-		public Triangle ()
-		{
+		private double _height , _width;
+
+		// propeties
+		public double width {
+			get{ return _width; }
+			set{ _width = (value >= 0) ? value : 1.00; }
 		}
+		public double height {
+			get{ return _height; }
+			set{ _height = (value >= 0) ? value : 1.00; }
+		}
+		public override string color
+		{
+			get { return _color; }
+		}
+
+
+		//constructor
+		public Triangle()
+		{
+			width = 1.00;
+			height = 1.00;
+			_color = "Gold";
+		}
+		public Triangle(string c,double w, double h)
+		{
+			width = w;
+			height = h;
+			_color = c;
+		}
+		public Triangle (Triangle a)
+		{
+			width = a.width;
+			height = a.height;
+			_color = a.color;
+		}
+
+		//method
+		public override double getArea()
+		{
+			return 0.5 * width * height;
+		}
+		public override double getPerimeter()
+		{
+			return Math.Sqrt (this.width * this.width + this.height * this.height) + this.width + this.height; 
+		}
+
+		public override string ToString ()
+		{
+			return String.Format ("[Triangle: base ={0}, height={1} area = {2}, color = {3}]", 
+				this.width, this.height, this.getArea(), this.color);
+		}
+
 	}
 }
 
