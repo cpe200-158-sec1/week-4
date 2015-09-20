7f0057e1908939322364e49540f2aa027893faa7
KRIANGKRAI KOLIBUT <kriangkai_k@cmu.ac.th>
2015-09-20 22:29:46 +0700

    570610551 LAB 402 Finish!!!

--- week4/Lab402/README.md
+++ week4/Lab402/README.md
@@ -46,11 +46,15 @@ Abstract class
 ## Questions:
 
 1. Are there still any redundant lines of code in your project? where?
+	Answer : No.
 2. Modify the project so that you can minimize or solve those redundancy.
+	Answer : We can call constractor in baseclass for solve redundancy.
 3. What happen if you uncomment line #27-28 in the Program.cs file? Why?
+	Answer : Error, because abstract class cant have constructor.
 4. If you uncomment line #30-24 in the Program.cs file, you will get some compile errors.
    Modify the classes and/or the program.cs without touching the "Main" method so that 
    the program will display the result as shown below:
+	Answer : Add and use printPerimeter method in mainclass but outside main method for show perimeter.
 
 ## Expected Output:
 
--- week4/Lab402/Rectangle.cs
+++ week4/Lab402/Rectangle.cs
@@ -2,11 +2,57 @@
 
 namespace Lab4
 {
-	public class Rectangle
+	public class Rectangle:Shape
 	{
-		public Rectangle ()
+        private double _Height;
+        private double _Width;
+        public double Height
+        {
+            get { return _Height; }
+            set { _Height = value; }
+        }
+        public double Width
+        {
+            get { return _Width; }
+            set { _Width = Math.Max(value , 1); }
+        }
+        public override string Color
+        {
+            get
+            {
+                return _Color;
+            }
+        }
+        public Rectangle ()
 		{
+            Height = 1.0;
+            Width = 1.0;
+            _Color = "Gold";
 		}
-	}
+        public Rectangle (string c,double w,double h)
+        {
+            Height = h;
+            Width = w;
+            _Color = c;
+        }
+        public Rectangle (Rectangle r)
+        {
+            Height = r.Height;
+            Width = r.Width;
+            _Color = r.Color;
+        }
+        public override double getArea()
+        {
+            return Height * Width;
+        }
+        public override double getPerimeter()
+        {
+            return (Height * 2) + (Width * 2);
+        }
+        public override string ToString()
+        {
+            return string.Format("[Rectangle: width={0}, height={1}, area={2}, color=\"{3}\"]", Width, Height, getArea(), Color);
+        }
+    }
 }
 
