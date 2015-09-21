42684c4f70d3c4ea714958b505911a28782eb0cf
cpe200-158-sec1-0547 <Kawephat_w@cmu.ac.th>
2015-09-21 21:11:37 +0700

    570610547 lab402 finish

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -4,8 +4,31 @@ namespace Lab4
 {
 	public abstract class Shape
 	{
+        public string _Color="gold";
+        public string Color
+        {
+            get;
+       
+        }
+        
+        public Shape(string a)
+        {
+            Color = a;
+        }
+
+        public abstract double getArea();
+        public abstract double Perimeter();
+        
+        
+
+        
+
+        
+
+        
 		public Shape ()
 		{
+
 		}
 	}
 }
--- week4/Lab402/Triangle.cs
+++ week4/Lab402/Triangle.cs
@@ -2,11 +2,93 @@
 
 namespace Lab4
 {
-	public class Triangle
+	public class Triangle:Shape
 	{
-		public Triangle ()
-		{
-		}
-	}
+        public double _Width;
+        public double _Height;
+
+        public double Width
+        {
+            get
+            {
+                return _Width;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _Width = value;
+                }
+                else
+                {
+                    _Width = 1.0;
+                }
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return _Height;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _Height = value;
+                }
+                else
+                {
+                    _Height = 1.0;
+                }
+            }
+        }
+        public Triangle(string a)
+        {
+            _Color = a;
+            _Height = 1.0;
+            _Width = 1.0;
+        }
+
+        public Triangle()
+        {
+            _Color = "gold";
+            _Height = 1.0;
+            _Width = 1.0;
+        }
+        public Triangle(string c, double w, double h)
+        {
+            _Color = c;
+            _Height = h;
+            _Width = w;
+        }
+
+        public Triangle(Rectangle a)
+        {
+            _Color = a.Color;
+            _Height = a._Height;
+            _Width = a._Width;
+        }
+
+        public override double getArea()
+        {
+            return 0.5*_Height*_Width;
+        }
+
+        public override double Perimeter()
+        {
+            return  Math.Sqrt(Math.Pow(_Width, 2) + Math.Pow(_Height, 2))+ _Width + _Height;
+        }
+
+        public override string ToString()
+        {
+            string output;
+            output = "[Triangle: base=" + _Width + ", height=" + _Height + ", area=" + getArea() + ", color=" + _Color + "]";
+
+            return output;
+        }
+
+    }
+
 }
 
