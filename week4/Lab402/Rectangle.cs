5b4eabda119ffdaade6dad0e19d2d3246bccb93a
mooitsara <moo_vespadark@hotmail.com>
2015-09-21 20:02:39 +0700

    570610621 Lab4.2  FULL

--- week4/Lab402/Rectangle.cs
+++ week4/Lab402/Rectangle.cs
@@ -2,11 +2,68 @@
 
 namespace Lab4
 {
-	public class Rectangle
+	public class Rectangle : Shape
 	{
-		public Rectangle ()
+        private double _width;
+        private double _height;
+        public double width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = (value > 0) ? value : 1.0;
+            }
+        }
+
+        public double height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height = (value > 0) ? value : 1.0;
+            }
+        }
+            
+		public Rectangle()
 		{
+            width = 1.0;
+            height = 1.0;
 		}
-	}
+
+        public Rectangle (string c , double ww , double hh)
+        {
+            color = c;
+            width = ww;
+            height = hh;
+        }
+
+        public Rectangle (Rectangle re)
+        {
+            color = re.Color;
+            width = re.width;
+            height = re.height;
+        }
+
+        public override double getArea()
+        {
+            return width * height;
+        }
+
+        public override double getPerimeter()
+        {
+            return 2 * (width + height);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Rectangle: width={0}, height={1}, area={2}, color={3}]", width, height, getArea(), color);
+        }
+    }
 }
 
--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -4,9 +4,27 @@ namespace Lab4
 {
 	public abstract class Shape
 	{
-		public Shape ()
-		{
-		}
-	}
+        protected string color;
+        
+        public Shape (string c = "gold")
+        {
+            color = c;
+        }
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+        }
+        public abstract double getArea();
+
+        public abstract double getPerimeter();
+
+        public override string ToString()
+        {
+            return string.Format("[color={0}]",Color);
+        }
+    }
 }
 
