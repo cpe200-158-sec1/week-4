a40c7a3c480add010b60b2cc865152925b7fc615
ANIRUT YOTSEAN <anirut_yotsean@cmu.ac.th>
2015-09-22 00:01:07 +0700

    570610616 LAB402 SSS

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -2,11 +2,11 @@
 
 namespace Lab4
 {
-	public abstract class Shape
-	{
-		public Shape ()
-		{
-		}
-	}
+    public abstract class Shape
+    {
+        protected string _Color;
+        public abstract string Color { get; }
+        public abstract double getArea();
+        public abstract double getPerimeter();
+    }
 }
-
