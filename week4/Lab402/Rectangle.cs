31b4f4d9567da359e043aff52ecb33e0f2ae47e2
nicethatchai <leonemidnice@gmail.com>
2015-09-21 23:20:49 +0700

    step 2-3

--- week4/Lab402/Rectangle.cs
+++ week4/Lab402/Rectangle.cs
@@ -2,11 +2,91 @@
 
 namespace Lab4
 {
-	public class Rectangle
+	public class Rectangle : Shape
 	{
-		public Rectangle ()
-		{
-		}
-	}
+        //# 2. Rectangle Class:
+
+        //## Properties:
+        //- double Width, Height: dimension of rectangle(>0, default=1.0)
+
+        private double _width;
+        private double _height;
+
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+
+            set
+            {
+                if (value > 0)
+                    Width = value;
+                else
+                    Width = 1.0;
+            }
+
+        }
+
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+
+            set
+            {
+                if (value > 0)
+                    Height = value;
+                else
+                    Height = 1.0;
+            }
+
+        }
+
+        //## Constructors:
+        //- Rectangle(): default
+        public Rectangle()
+        {
+            Color = "gold";
+            Width = 1.0;
+            Height = 1.0;
+        }
+        //- Rectangle(string, double, double): takes color, width, height as parameters
+        public Rectangle(string inColor, double inWidth, double inHeight)
+        {
+            Color = inColor;
+            Width = inWidth;
+            Height = inHeight;
+        }
+        //- Rectangle(Rectangle): copy constructor
+        public Rectangle(Rectangle inRec)
+        {
+            Color = inRec.Color;
+            Width = inRec.Width;
+            Height = inRec.Height;
+        }
+
+        //## Methods:
+        //- getArea(): get area of a rectangle
+        public override double getArea()
+        {
+            return Width * Height;
+        }
+
+        public override double getPerimeter()
+        {
+            return 2.0 * (Width + Height);
+        }
+        //- override ToString() method
+        public override string ToString()
+        {
+            return string.Format("[Rectangle: width={0}, height={1}, area={2}, color=\"{3}\"]", Width, Height, getArea(), Color);
+        }
+
+    }
+}
 }
 
--- week4/Lab402/Triangle.cs
+++ week4/Lab402/Triangle.cs
@@ -2,11 +2,84 @@
 
 namespace Lab4
 {
-	public class Triangle
-	{
-		public Triangle ()
-		{
-		}
-	}
+    public class Triangle : Shape
+    {
+        // # 3. Triangle Class:
+
+        //## Properties:
+        //- double Width, Height: dimension of rectangle(>0, default=1.0)
+        private double _width;
+        private double _height;
+
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+
+            set
+            {
+                if (value > 0) Width = value;
+                else Width = 1.0;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+
+            set
+            {
+                if (value > 0) Height = value;
+                else Height = 1.0;
+            }
+        }
+
+        //## Constructors:
+        //- Triangle() : default
+        public Triangle()
+        {
+            Color = "gold";
+            Width = 1.0;
+            Height = 1.0;
+        }
+        
+        //- Triangle(string, double, double) : takes color, width, height as parameters
+        public Triangle (string inColor, double inWidth, double inHeight)
+        {
+            Color = inColor;
+            Height = inHeight;
+            Width = inWidth;
+        }
+        //- Triangle(Rectangle) : copy constructor
+        public Triangle (Triangle inTri)
+        {
+            Color = inTri.Color;
+            Width = inTri.Width;
+            Height = inTri.Height;
+        }
+
+        //## Methods:
+        //- getArea(): get area of a triangle
+        public override double getArea()
+        {
+            return 0.5 * Width * Height;
+        }
+
+        public override double getPerimeter()
+        {
+            return Width + Height + Math.Sqrt((Width * Width) + (Height * Height));
+        }
+
+        //- override ToString() method
+        public override string ToString()
+        {
+            return string.Format("[Triangle: base={0}, height={1}, area={2}, color=\"{3}\"]", Width, Height, getArea(), Color);
+        }
+    }
 }
 
