621773d6b02f180a36ac40b0bb152480e229affc
drivemontri <auntafung@hotmail.com>
2015-09-21 22:19:16 +0700

    0594 Lab402

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -4,9 +4,30 @@ namespace Lab4
 {
 	public abstract class Shape
 	{
-		public Shape ()
-		{
-		}
-	}
+        private string _color ;
+
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+        }
+
+        public Shape(string color = "gold")
+        {
+            _color = color;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}", Color);
+        }
+
+        public abstract double getArea();
+
+        public abstract double getPerimeter();
+
+    }
 }
 
