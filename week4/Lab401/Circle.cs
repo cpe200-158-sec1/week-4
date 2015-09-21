856f29d1b5b059a5a29e9e49dee9138f5859d1dd
RessYeiei <dustche_manage1996@hotmal.com>
2015-09-21 23:26:17 +0700

    lab 401 0608

--- week4/Lab401/Circle.cs
+++ week4/Lab401/Circle.cs
@@ -2,11 +2,50 @@
 
 namespace Lab4
 {
-	public class Circle
+	public class Circle : Point
 	{
-		public Circle ()
-		{
-		}
-	}
+        private double Radius;
+        public double radius
+        {
+            get { return Radius; }
+            set { Radius = value; }
+        }
+        public Circle()
+        {
+            x = 0.0;
+            y = 0.0;
+            radius = 1.0;
+        }
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        public Circle(double a, double b, double r)
+        {
+            x = a;
+            y = b;
+            radius = r;
+        }
+        public Circle(Circle c)
+        {
+            x = c.x;
+            y = c.y;
+            radius = c.radius;
+        }
+        public void setCircle(double a, double b, double r)
+        {
+            x = a;
+            y = b;
+            radius = r;
+        }
+        public virtual double getArea()
+        {
+            return Math.PI * radius * radius;
+        }
+        public override string ToString()
+        {
+            return String.Format("[Circle: center({0},{1}), radius={2}, area={3}", this.x, this.y, this.radius, this.getArea());
+        }
+    }
 }
 
