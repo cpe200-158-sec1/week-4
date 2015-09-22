33deac988d603e4620b40432a63260da6617bfed
worawalan10 <jus2n.pls@live.com>
2015-09-22 15:54:44 +0700

    Lab401 - 0598

--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -2,11 +2,43 @@
 
 namespace Lab4
 {
-	public class Point
-	{
-		public Point ()
-		{
-		}
-	}
-}
+    public class Point
+    {
+        private double _X;
+        private double _Y;
 
+        public double X
+        {
+            get { return _X; }
+            set { _X = value; }
+        }
+
+        public double Y
+        {
+            get { return _Y; }
+            set { _Y = value; }
+        }
+
+        public Point()
+        {
+            X = 0.0;
+            Y = 0.0;
+        }
+
+        public Point(Point a)
+        {
+            X = a.X;
+            Y = a.Y;
+        }
+
+        public Point(double a, double b)
+        {
+            X = a;
+            Y = b;
+        }
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", X, Y);
+        }
+    }
+}
