63b5840c08a431e88996aab2c1e9c993bed6503f
Nontapat <thenut_project@hotmail.com>
2015-09-22 17:34:39 +0700

    570610580 W4

--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
-namespace Lab4
+namespace Lab401
 {
 	public class Point
 	{
-		public Point ()
-		{
-		}
-	}
+        private double _x;
+        private double _y;
+
+        public double X
+        {
+            get { return _x; }
+            set { _x= value;}
+        }
+
+        public double Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Point( double x, double y)
+        {
+            X = x;
+            Y = y;
+
+        }
+        public Point(Point p)
+        {
+            X = p.X;
+            Y = p.Y;
+
+
+        }
+
+        public override string ToString()
+        {
+            return "Point ("+X+","+Y+" )";
+        }
+
+    }
 }
 
