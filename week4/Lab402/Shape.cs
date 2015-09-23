04d50edb47a25a8abda2292e5f87809ab98459f1
worawalan10 <jus2n.pls@live.com>
2015-09-23 12:47:01 +0700

    Lab 402

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿ using System;
 
 namespace Lab4
 {
 	public abstract class Shape
 	{
-		public Shape ()
+        private string _Color;
+        public abstract double getArea();
+        public abstract double getPerimeter();
+
+        public string Color
+        {
+            get { return _Color; }
+        }
+
+		public Shape ( string color = "gold")
 		{
+            _Color = color;
 		}
-	}
+
+        public override string ToString()
+        {
+            return string.Format("0", Color);
+        }
+    }
 }
 
