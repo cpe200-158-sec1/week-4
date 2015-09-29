ec3a08cc1ad24d4596dd9b7944688f5ec36c2d8a
Nontapat <thenut_project@hotmail.com>
2015-09-29 15:49:39 +0700

    580610580 402

--- week4/Lab402/Rectangle.cs
+++ week4/Lab402/Rectangle.cs
@@ -1,12 +1,85 @@
 ﻿using System;
 
-namespace Lab4
+namespace Lab402
 {
-	public class Rectangle
+	public  class Rectangle  : Shape
 	{
-		public Rectangle ()
-		{
-		}
-	}
+		
+             //## Properties:
+            // -double Width,Height: dimension of rectangle(> 0, default= 1.0)
+
+        public override string Color
+        {
+            get
+            {
+                return _color;
+            }
+        }
+
+        private double _width;
+             private double _height;
+
+             public double Width
+             {
+                 get { return _width; }
+                 set
+                    {
+                     if (value < 0)
+                    value = 1.0;
+                       _width = value;
+
+                     }
+              }
+              public double Height
+                {
+                    get { return _height; }
+                    set
+                    {
+                if (value < 0)
+                    value = 1.0;
+                _height = value;
+
+                     }
+                }
+
+
+
+        //## Constructors:
+        //- Rectangle(): default
+        //- Rectangle(string, double, double): takes color, width, height as parameters
+        //    - Rectangle(Rectangle): copy constructor
+        public Rectangle(string color = "gold", double w = 1.0, double h = 1.0)
+        {
+            //Defalt : [Rectangle: width=1, height=1, area=1, color="gold"]
+            _color = color;
+            Width = w;
+            Height = h;
+        }
+        public Rectangle(Rectangle r)
+        {
+            _color = r._color;
+            Width = r.Width;
+            Height = r.Height;
+        }
+        //## Methods:
+        //-getArea(): get area of a rectangle
+        //- override ToString() method
+        public override double getArea()
+        {
+            return Height * Width;
+        }
+
+        public override double getPerimeter()
+        {
+            return 2.0 * (Height + Width);
+        }
+        public override string ToString()
+        {
+            //[Rectangle: width=4, height=5, area=20, color="red"]
+            return "[Rectangle : width=" + Width + ", height=" + Height + ", area=" + getArea() + ", color=" + Color + "]";
+        }
+
+    }
 }
+
 
